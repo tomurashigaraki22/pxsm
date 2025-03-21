cabcd0be4561cca0f8f0dbce9d69477fa845b9a4
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2025-03-21 21:11:08 +0100

    new

--- src/config.jsx
+++ src/config.jsx
@@ -1,4 +1,4 @@
 // API configuration
-const API_URL = 'http://localhost:1245'; // Flask default port is 5000
+const API_URL = 'http://172.20.10.5:1245'; // Flask default port is 5000
 
 export { API_URL };
