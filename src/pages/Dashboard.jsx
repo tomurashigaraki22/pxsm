bffb6d2c20b42f7915f7654fdf8c91fa524155e3
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2025-04-06 23:52:58 +0100

    neww

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -216,6 +216,7 @@ useEffect(() => {
   };
 
   const calculateRateWithProfit = (baseRate) => {
+    print("Base rate per 1000: ", baseRate)
     if (parseInt(baseRate) > 10000){
       return (baseRate * 1.15).toFixed(2); // Multiply by 1.15 and round to 2 decimal places
     }
