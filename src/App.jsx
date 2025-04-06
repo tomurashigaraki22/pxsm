0c8b409028f4654d8e2e117f3229a513c4bc19db
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2025-04-07 00:41:44 +0100

    neww

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { ProtectedRoute, PublicRoute } from './components/ProtectedRoute';
 import Home from './pages/Home';
