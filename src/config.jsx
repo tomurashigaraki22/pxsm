0fe0ce026dc2b555bfd672b86f87c4c509c184a8
tomurashigaraki22 <emmanuelhudson355@gmail.com>
2025-06-28 11:53:52 +0100

    new

--- src/config.jsx
+++ src/config.jsx
@@ -1,4 +1,4 @@
 // API configuration
-const API_URL = 'https://px-backend-atfl.onrender.com'; // Flask default port is 5000
+const API_URL = 'https://pxbackend.pxxl.tech'; // Flask default port is 5000
 // const API_URL = "http://192.168.13.148:1245"
 export { API_URL };
